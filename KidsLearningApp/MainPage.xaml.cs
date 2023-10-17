fddd3ff500eefc0d173a4448873abb4e985e9573
Student <Student@205-45>
2023-10-17 16:02:11 +0800

    加入專案檔案。

---
+++ KidsLearningApp/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace KidsLearningApp
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            ImageButton button = (ImageButton)sender;
+            Navigation.PushAsync(new KidsLearningDetailPage(button.CommandParameter.ToString()));
+        }
+        
+    }
+}
